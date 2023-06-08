147e455dff77df9e519a7a386cbdef5d0c6144c1
dieum512 <akonkwadieum222@gmail.com>
2023-06-09 00:56:00 +0200

    Correct the linter errors

--- spec/book.rb
+++ spec/book.rb
@@ -1,15 +1,15 @@
 require_relative 'rental'
 
 class Book
-    attr_accessor :title, :author, :rental
+  attr_accessor :title, :author, :rental
 
-    def initialize(title, author, rental)
-        @title = title
-        @author = author
-        @rental = []
-    end
+  def initialize(title, author)
+    @title = title
+    @author = author
+    @rental = []
+  end
 
-    def add_rental(person, date)
-        Rental.new(person, self, date)
-    end
+  def add_rental(person, date)
+    Rental.new(person, self, date)
+  end
 end
--- spec/rental.rb
+++ spec/rental.rb
@@ -1,12 +1,12 @@
 class Rental
-    attr_accessor :date, :book, :person
+  attr_accessor :date, :book, :person
 
-    def initialize(date, book, person)
-        @date = date
-        @book = book
-        @person = person
+  def initialize(date, book, person)
+    @date = date
+    @book = book
+    @person = person
 
-        book.rental << self
-        person.rental << self
-    end
+    book.rental << self
+    person.rental << self
+  end
 end
