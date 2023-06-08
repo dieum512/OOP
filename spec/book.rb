f4254101ad053392dc7957678111e0d22d22fa68
dieum512 <akonkwadieum222@gmail.com>
2023-06-09 00:33:33 +0200

    Update the book file

--- spec/book.rb
+++ spec/book.rb
@@ -1,9 +1,15 @@
+require_relative 'rental'
+
 class Book
-    attr_accessor :title, :author
+    attr_accessor :title, :author, :rental
 
     def initialize(title, author, rental)
         @title = title
         @author = author
         @rental = []
+    end
+
+    def add_rental(person, date)
+        Rental.new(person, self, date)
     end
 end
