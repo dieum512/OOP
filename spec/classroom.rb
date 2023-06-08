5de0dcc3607959a6d6fbd90bf8cb3a800c21217d
dieum512 <akonkwadieum222@gmail.com>
2023-06-09 00:20:01 +0200

    Create the classroom class

---
+++ spec/classroom.rb
@@ -0,0 +1,12 @@
+class Classroom
+    attr_accessor :label, :students
+
+    def initialize(label)
+        @label = label
+    end
+
+    def add_student(student)
+        @student << student
+        student.classroom = self
+    end
+end
