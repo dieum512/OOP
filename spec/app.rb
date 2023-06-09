752d4a50869f15a307b0dc51181b6007138169f2
dieum512 <akonkwadieum222@gmail.com>
2023-06-09 16:58:12 +0200

    Create the app class

---
+++ spec/app.rb
@@ -0,0 +1,19 @@
+require_relative 'student'
+require_relative 'teacher'
+require_relative 'book'
+require_relative 'rental'
+
+class App
+    def initialize
+        @books = []
+        @rentals = []
+        @people = []
+    end
+
+    # def list_all_books
+    #     @books.each do |book|
+    #         puts "Title: #{book.title}, Author: #{book.author}"
+    #     end
+    # end
+    
+end
