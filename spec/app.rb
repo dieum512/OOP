9428f868692107bf4967caef161a8f00983ee167
dieum512 <akonkwadieum222@gmail.com>
2023-06-09 16:59:00 +0200

    Create a method that list all books

--- spec/app.rb
+++ spec/app.rb
@@ -10,10 +10,10 @@ class App
         @people = []
     end
 
-    # def list_all_books
-    #     @books.each do |book|
-    #         puts "Title: #{book.title}, Author: #{book.author}"
-    #     end
-    # end
+    def list_all_books
+        @books.each do |book|
+            puts "Title: #{book.title}, Author: #{book.author}"
+        end
+    end
     
 end
