d0281362718e88e2d0176452e978b726fd6350a9
dieum512 <akonkwadieum222@gmail.com>
2023-06-09 18:36:07 +0200

    Add the menu to boe displayed

--- spec/main.rb
+++ spec/main.rb
@@ -6,4 +6,22 @@ class Main
     def initialize(app)
         @app = app
     end
+
+    # def main
+    #     loop do
+
+    #     end
+    # end
+
+    def display_menu
+        puts 'please choose an option by entering a nunmber'
+
+        puts '1 - list all books'
+        puts '2 - List all people'
+        puts '3 - Create a person'
+        puts '4 - Create a book'
+        puts '5 - Create a rental'
+        puts '6 - List all the rental for a given person id'
+        puts '7 - Exit'
+    end
 end
